148d577f3dc254722351f7700efa49a09307e40c
MonaHasan95 <54250105+MonaHasan95@users.noreply.github.com>
2019-12-12 20:05:31 +0200

    Update default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,66 +1,109 @@
+<style>
+#searchBar {
+  width: 600px;
+  height: 50px;
+  padding: 10px;
+  background-color: #29a79b;
+  color: white;
+}
+#searchBar::placeholder {
+  color: white;
+}
+#searchBar:focus {
+  background-color: #2eb6a8;
+}
+#searchBtn {
+  width: 70px;
+  height: 50px;
+  padding: 10px;
+  background-color: #05887b;
+  color: white;
+  margin-right: 15px;
+}
+#searchBtn:hover {
+  background-color: #057d71;
+}
+#p {
+  margin-top: 20px;
+  margin-right: 20px;
+}
+#pic {
+  width: 40px;
+  height: 40px;
+  margin-left: 10px;
+  border-radius: 50%;
+  background-image: url('../assets/photo-1503443207922-dff7d543fd0e.jpg');
+  background-size: cover;
+}
+</style>
 <template>
-  <v-app dark>
-    <v-navigation-drawer
-      v-model="drawer"
-      :mini-variant="miniVariant"
-      :clipped="clipped"
+  <v-app style="background-color:azure">
+    <v-app-bar
+      :clipped-left="clipped"
       fixed
       app
+      style="background-color:#009688;"
     >
-      <v-list>
-        <v-list-item
-          v-for="(item, i) in items"
-          :key="i"
-          :to="item.to"
-          router
-          exact
-        >
-          <v-list-item-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title v-text="item.title" />
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    <v-app-bar :clipped-left="clipped" fixed app>
+      <!--leftpanel-->
       <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-btn icon @click.stop="miniVariant = !miniVariant">
+
+      <!--arrow
+      <v-btn @click.stop="miniVariant = !miniVariant" icon>
         <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
       </v-btn>
-      <v-btn icon @click.stop="clipped = !clipped">
+
+      window
+      <v-btn @click.stop="clipped = !clipped" icon>
         <v-icon>mdi-application</v-icon>
       </v-btn>
-      <v-btn icon @click.stop="fixed = !fixed">
+
+      Minus sign
+      <v-btn @click.stop="fixed = !fixed" icon>
         <v-icon>mdi-minus</v-icon>
-      </v-btn>
-      <v-toolbar-title v-text="title" />
+      </v-btn>-->
+
+      <!--Title-->
+      <font-awesome-icon
+        :icon="['fas', 'robot']"
+        style="margin-right:50px;"
+        size="lg"
+      />
+      <v-form>
+        <span>
+          <input
+            id="searchBar"
+            name="searchKeyword"
+            type="text"
+            placeholder="Search your apps"
+          />
+          <button id="searchBtn">Search</button>
+        </span>
+      </v-form>
       <v-spacer />
-      <v-btn icon @click.stop="rightDrawer = !rightDrawer">
+      <p id="p">
+        <span>
+          <font-awesome-icon :icon="['fas', 'users']" size="lg" />
+        </span>
+        300
+      </p>
+      <p id="p">
+        <span>
+          <font-awesome-icon :icon="['fas', 'th']" size="lg" />
+        </span>
+        653
+      </p>
+      <p style="margin-top: 20px;">Amr Sobhy</p>
+      <div id="pic"></div>
+      <!--rightpanel
+      <v-btn @click.stop="rightDrawer = !rightDrawer" icon>
         <v-icon>mdi-menu</v-icon>
-      </v-btn>
+      </v-btn>-->
     </v-app-bar>
     <v-content>
       <v-container>
         <nuxt />
       </v-container>
     </v-content>
-    <v-navigation-drawer v-model="rightDrawer" :right="right" temporary fixed>
-      <v-list>
-        <v-list-item @click.native="right = !right">
-          <v-list-item-action>
-            <v-icon light>
-              mdi-repeat
-            </v-icon>
-          </v-list-item-action>
-          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    <v-footer :fixed="fixed" app>
-      <span>&copy; 2019</span>
-    </v-footer>
   </v-app>
 </template>
 
