a70fdfef431c8208d2d18cfd079861a82a2e7461
MonaHasan95 <54250105+MonaHasan95@users.noreply.github.com>
2019-12-12 20:02:50 +0200

    Add files via upload

---
+++ pages/VueChartJS.vue
@@ -0,0 +1,20 @@
+<template>
+  <section class="container">
+    <div class="columns">
+      <div class="column">
+        <line-chart></line-chart>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import LineChart from '@/components/LineChart'
+
+export default {
+  name: 'VueChartJS',
+  components: {
+    LineChart
+  }
+}
+</script>
